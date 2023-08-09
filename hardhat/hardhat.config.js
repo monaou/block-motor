c47b4e02cd14552025cf321ede0994a292e83724
monaou <monaou.freelance@gmail.com>
2023-08-09 19:45:09 +0900

    feat : contractt

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -2,5 +2,21 @@ require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.19",
+  solidity: "0.8.0",
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+    },
+    hmvtest: {
+      url: 'https://rpc.sandverse.oasys.games',
+      chainId: 20197,
+      gasPrice: 0,
+      accounts: [
+        '0xa267530f49f8280200edf313ee7af6b827f2a8bce2897751d06a843f644967b1'
+        // Provide your private key here
+        // Remove comment out to use value from secrets.json
+        //PRIVATE_KEY
+      ],
+    },
+  },
 };
--- hardhat/scripts/deploy.js
+++ hardhat/scripts/deploy.js
@@ -1,33 +1,31 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-const hre = require("hardhat");
+const fs = require('fs');
+
+// Type your own PUBLIC key in here
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  const ContractFactory = await ethers.getContractFactory("CarNFT");
+  const contract = await ContractFactory.deploy();
 
-  const lockedAmount = hre.ethers.parseEther("0.001");
+  await contract.deployed();
+  console.log("Dino Runner contract deployed to:", contract.address);
 
-  const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
+  // アドレスをオブジェクトとして作成
+  const addressObject = {
+    contractAddress: contract.address
+  };
 
-  await lock.waitForDeployment();
+  // JSON形式でオブジェクトを文字列に変換
+  const jsonData = JSON.stringify(addressObject, null, 2);
 
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+  // JSONデータをファイルに書き込む
+  fs.writeFileSync('contractAddress.json', jsonData);
+
+  console.log("Dino Runner address saved to: contractAddress.json");
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
